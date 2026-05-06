cf6fb11cafef5bbd5c22d70f3fdce8355250a739
Tanmay007-okay <tanmayjoshi347@gmail.com>
2026-05-06 05:00:48 +0000

    Added product page

---
+++ src/components/Product.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Product = () => (
+  <div>
+    <h2>Product Component</h2>
+  </div>
+);
+
+// THIS IS THE CRITICAL LINE MISSING OR INCORRECT
+export default Product;
