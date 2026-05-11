b0e10b33116e90cc84f45a948fc16a90b145574c
Tanmay007-okay <tanmayjoshi347@gmail.com>
2026-05-11 07:45:12 +0000

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
 
-// Notice the clean alias "neetoui"
-
+// Import our three "buildings" (components)
+import Home from "./components/Home";
+import PageNotFound from "./components/PageNotFound";
 import Product from "./components/Product";
 
-const App = () => <Product />;
+const App = () => (
+  <>
+    {/* 1. THE NAVIGATION BAR */}
+    <div className="mx-4 mt-4 flex space-x-2">
+      <NavLink exact activeClassName="underline font-bold" to="/">
+        Home
+      </NavLink>
+      <NavLink exact activeClassName="underline font-bold" to="/product">
+        Product
+      </NavLink>
+    </div>
+    {/* 2. THE ROUTER DISPLAY CASE */}
+    <Switch>
+      {/* If the URL is exactly "/", show the Home component */}
+      <Route exact component={Home} path="/" />
+      {/* If the URL is exactly "/product", show the Product component */}
+      <Route exact component={Product} path="/product" />
+      {/* If the URL is ANYTHING ELSE (*), show the Error Page */}
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
+);
 
 export default App;
