9cb5574ee6c584efac0d9628d6cd338b4ff60054
Tanmay007-okay <tanmayjoshi347@gmail.com>
2026-05-15 08:15:55 +0000

    Implemented caching of country and state data using persistQueryClient

--- src/utils/queryClient.js
+++ src/utils/queryClient.js
@@ -1,4 +1,8 @@
+import { QUERY_KEYS } from "constants/query";
+
 import { QueryClient, QueryCache } from "react-query";
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental";
+import { persistQueryClient } from "react-query/persistQueryClient-experimental";
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache(),
@@ -7,6 +11,25 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       staleTime: 3600000,
     },
+  },
+});
+
+// 1. Create the engine that knows how to save data to the browser
+const localStoragePersistor = createWebStoragePersistor({
+  storage: window.localStorage,
+});
+
+// 2. Attach it to our React Query instance
+persistQueryClient({
+  queryClient,
+  persistor: localStoragePersistor,
+  maxAge: Infinity, // Keep this data forever (or until the user clears their browser cache)
+  dehydrateOptions: {
+    // 3. ONLY save the Countries and States APIs to the hard drive
+    shouldDehydrateQuery: ({ queryKey }) =>
+      [QUERY_KEYS.COUNTRIES, QUERY_KEYS.STATES].some((key) =>
+        queryKey.includes(key)
+      ),
   },
 });
 
